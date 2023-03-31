5337f9dac2d003d98c12aa4b79c98c652e1e5192
antiLimited <antilimitedmail@gmail.com>
2023-03-31 14:51:00 -0400

    updates :3

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+let termText = document.getElementById("terminal-text").innerHTML
 
+let keys = [
+    
+    {
+        "key": "Enter", "text": "<br>"
+    }
+]
 
 document.addEventListener('keydown', function(event) {
+    for (let i in keys) {
+        i = keys[i]
+        if (i.key == event.key) {
+            termText = termText + i.text
+        }
+    }
     console.log(event.key)
-    document.write(event.key)
+    document.getElementById("terminal-text").innerHTML = termText
 })
