230355b9dd87d51f9740ad90fe3855f55b62672e
antiLimited <antilimitedmail@gmail.com>
2023-04-01 11:35:01 -0400

    i hate myself :3

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 let term = document.getElementById("terminal-text")
 
+// document.addEventListener('keydown', function (event) {
+//     let termText = term.innerHTML
+//     if (event.key.length == 1) {
+//         termText = termText + event.key
+//         term.innerHTML = termText
+//     } else if (event.key == "Enter"){
+//         termText = termText + "<br>[user@antilimit.dev ~]$ "
+//         term.innerHTML = termText
+//     } else if (event.key == "Backspace"){
+//         termText = termText.slice(0, -1)
+//         term.innerHTML = termText
+//     }
+//     document.getElementById("page-bottom").scrollIntoView()
+// })
+
+function renderInput(i) {
+    term.innerHTML = prevCont + i
+}
+
+function sendCommand(i) {
+    input = ""
+    prevCont = prevCont + "<br>[user@antilimit.dev ~]$ " + i
+    term.innerHTML = term.innerHTML + prevCont
+}
+
+let prevCont = "[user@antilimit.dev ~]$ "
+let input = ""
+
 document.addEventListener('keydown', function (event) {
-    let termText = term.innerHTML
     if (event.key.length == 1) {
-        termText = termText + event.key
-        term.innerHTML = termText
-    } else if (event.key == "Enter"){
-        termText = termText + "<br>[user@antilimit.dev ~]$ "
-        term.innerHTML = termText
-    } else if (event.key == "Backspace"){
-        termText = termText.slice(0, -1)
-        term.innerHTML = termText
+        input = input + event.key
+    } else if (event.key == "Enter") {
+        sendCommand(input)
+    } else if (event.key == "Backspace") {
+        input = input.slice(0, -1)
     }
+    console.log(input)
+    renderInput(input)
+    document.getElementById("page-bottom").scrollIntoView()
 })
+debugger
