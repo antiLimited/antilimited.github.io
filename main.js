3c80a23aff3497768ff29146fbf2f2feabd4c0f9
antiLimited <antilimitedmail@gmail.com>
2023-03-31 22:38:56 -0400

    more changes :3

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
+let term = document.getElementById("terminal-text")
+
 document.addEventListener('keydown', function (event) {
-    let termText = document.getElementById("terminal-text").innerHTML
+    let termText = term.innerHTML
     if (event.key.length == 1) {
         termText = termText + event.key
-        console.log(event.key)
-        document.getElementById("terminal-text").innerHTML = termText
+        term.innerHTML = termText
     } else if (event.key == "Enter"){
         termText = termText + "<br>"
-        console.log(event.key)
-        document.getElementById("terminal-text").innerHTML = termText
+        term.innerHTML = termText
     } else if (event.key == "Backspace"){
         termText = termText.slice(0, -1)
-        console.log(event.key)
-        document.getElementById("terminal-text").innerHTML = termText
+        term.innerHTML = termText
     }
 })
