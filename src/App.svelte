b1d020d471f303efe1b2da9bd358473cbab31e10
antiLimit <webmaster@antilimit.dev>
2024-12-13 16:24:05 -0500

    AAAAAAAAAAAA :3

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,4 @@
-<meta content="antilimit.dev" property="og:title" />
-<meta content="haha i bet youre looking at this through an embed instead of clicking the link you dumb dumb stupid dumb idiot" property="og:description" />
 <link rel="stylesheet" href="src/style.css">
-<title>antiLimit's Website</title>
   
 <div class="navbar">
     <a href="#">Profiles</a>
