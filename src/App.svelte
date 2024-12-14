e35918038cf437098ffed2396b6d0fbdae50dc07
antiLimit <webmaster@antilimit.dev>
2024-12-13 20:11:51 -0500

    a lot of changes :3

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,78 @@
 <link rel="stylesheet" href="src/style.css">
-  
+
 <div class="navbar">
-    <a href="#">Profiles</a>
-    <a href="#">Test Page</a>
+    <a href="./" id="navbar-first">Home</a>
+    <div class="navbar-sep"></div>
+    <a href="./" id="navbar-last">Profiles</a>
+    <div class="navbar-sep"></div>
+    <a href="./" id="navbar-last">Projects</a>
 </div>
 
 <div class="main-content">
     <h1>hello</h1>
-    <p>line\nline</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
+    <p>line</p>
 </div>
 
