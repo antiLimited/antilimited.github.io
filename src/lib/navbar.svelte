83ee0674f067bf7460c319da8251cb865b4fbb0c
antiLimit <webmaster@antilimit.dev>
2024-12-14 23:40:38 -0500

    navbar updates + reorg + basic js animation :3

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+    addEventListener("scroll", () => {
+        if (window.scrollY == 0) {
+            document.querySelector(".navbar").classList.add("up");
+            document.querySelector(".navbar").classList.remove("down");
+        } else {
+            document.querySelector(".navbar").classList.add("down");
+            document.querySelector(".navbar").classList.remove("up");
+        }
+    });
+</script>
+
+<div class="navbar">
+    <a href="./" id="navbar-first">HOME</a>
+    <div class="navbar-sep"></div>
+    <a href="./" id="navbar-last">PROFILES</a>
+    <div class="navbar-sep"></div>
+    <a href="./" id="navbar-last">PROJECTS</a>
+</div>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { mount } from "svelte"
+import "./assets/style.css"
 import App from "./App.svelte";
 
 const app = mount(App, {
