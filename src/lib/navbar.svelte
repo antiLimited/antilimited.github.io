5b2b40e41553ee3aacaa42671ff5c02bea1a60a6
antiLimit <webmaster@antilimit.dev>
2024-12-16 00:30:40 -0500

    i will deal with this at a later date actually :3

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -20,32 +20,12 @@
         }
     });
 
-    function assignRows(navbar) {
-        let row = 0;
-        let odd = true;
-        [...navbar.children].forEach((el) => {
-            // remove old classes every time the screen gets resized and set back to default
-            el.className = "navbar-link";
-            if (!el.previousElementSibling || el.offsetLeft < el.previousElementSibling.offsetLeft) {
-                row++;
-                odd = !odd;
-            }
-            // adds a class of row1, row2, row3
-            el.classList.add(`navbar-row${row}`, `${odd ? "navbar-wrapped" : "navbar-not-wrapped"}`);
-        });
-    };
-    const observer = new ResizeObserver((entries) => {
-        entries.forEach((entry) => {
-            assignRows(entry.target);
-        });
-    });
-
 </script>
 
 <div class="navbar">
     <a href="#" on:click={() => $curPage = "home"}>HOME</a>
-    <!-- <div class="navbar-sep"></div> -->
+    <div class="navbar-sep"></div>
     <a href="#" on:click={() => $curPage = "profiles"}>PROFILES</a>
-    <!-- <div class="navbar-sep"></div> -->
+    <div class="navbar-sep"></div>
     <a href="#" on:click={() => $curPage = "projects"}>PROJECTS</a>
 </div>
