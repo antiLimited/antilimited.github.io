f93286bc0f24bf88bc5863992d8dc51b04dd7a45
antiLimit <webmaster@antilimit.dev>
2024-12-17 08:11:46 -0500

    navbar script fully commented :3

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,51 +1,54 @@
 <script>
     import { curPage } from "../stores.js";
 
+    // Define empty variable to set in DOMContentLoaded event
     let navbar;
+    let navbarLinks = [];
 
-    let navbarContents = [];
-
-    let DOMloaded = false
+    // Runs on page load
     document.addEventListener('DOMContentLoaded', function() {
         console.log("DOM loaded!");
-        navbar = document.querySelector(".navbar");
+        navbar = document.querySelector(".navbar"); // Sets navbar to refer to top level navbar div
         
         if (window.scrollY == 0) {
-            navbar.classList.add("navbar-init-up")
+            navbar.classList.add("navbar-init-up"); // Set starting position docked on init (no anim)
         } else {
-            navbar.classList.add("navbar-init-down")
+            navbar.classList.add("navbar-init-down"); // Set starting position floating on init (no anim)
         };
 
-        navbarContents = Array.from(document.getElementsByClassName("navbar-link"));
-        DOMloaded = true;
-        highlightCurPage(true);
-        
+        navbarLinks = Array.from(document.getElementsByClassName("navbar-link")); // Set navbarLinks to an array of all links in top level navbar div
+        highlightCurPage(true); // Call highlight with init true to prevent animation for inactive links
     }, false);
 
+    // Call on any scroll events on page
     addEventListener("scroll", () => {
         if (window.scrollY == 0) {
+            // Set navbar classes to appear docked (up) if scrolled up fully
             navbar.classList.add("navbar-up");
             navbar.classList.remove("navbar-down");
         } else {
+            // Set navbar classes to appear floating (down) otherwise
             navbar.classList.add("navbar-down");
             navbar.classList.remove("navbar-up");
-        }
+        };
     });
 
     // Highlight active page link in navbar based on $curPage store
     function highlightCurPage(init, empty) {
         // Loop over all links inside of navbar
-        for (let i = 0; i < navbarContents.length; i++) {
-            const link = navbarContents[i];
+        for (let i = 0; i < navbarLinks.length; i++) {
+            const link = navbarLinks[i];
+
+            // Check if current page link matches $curPage
             if ($curPage == link.innerHTML.toLowerCase()) {
-                link.classList.remove("navbar-inactive", "navbar-inactive-pgload");
-                link.classList.add("navbar-active");
+                link.classList.remove("navbar-inactive", "navbar-inactive-pgload"); // Remove old inactive classes
+                link.classList.add("navbar-active"); // Add active class
             } else {
-                link.classList.remove("navbar-active");
+                link.classList.remove("navbar-active"); // Remove old active class
                 if (init) {
-                    link.classList.add("navbar-inactive-pgload");
+                    link.classList.add("navbar-inactive-pgload"); // Added if first call after DOM load, prevents active to inactive anim on page load
                 } else {
-                    link.classList.add("navbar-inactive");
+                    link.classList.add("navbar-inactive"); // Added on any other call
                 };
             };
         };
@@ -54,9 +57,9 @@
 </script>
 
 <div class="navbar">
-    <a href="#" class="navbar-link" onclick={() => $curPage = "home"}>HOME</a>
+    <a href="javascript:;" class="navbar-link" onclick={() => $curPage = "home"}>HOME</a>
     <div class="navbar-sep"></div>
-    <a href="#" class="navbar-link" onclick={() => $curPage = "profiles"}>PROFILES</a>
+    <a href="javascript:;" class="navbar-link" onclick={() => $curPage = "profiles"}>PROFILES</a>
     <div class="navbar-sep"></div>
-    <a href="#" class="navbar-link" onclick={() => $curPage = "projects"}>PROJECTS</a>
+    <a href="javascript:;" class="navbar-link" onclick={() => $curPage = "projects"}>PROJECTS</a>
 </div>
