9f1f57ec0e8e667a46f58b2904329985c25d3ede
antiLimit <webmaster@antilimit.dev>
2024-12-18 09:57:53 -0500

    class is over also more stuff done :3

---
+++ src/lib/pages/projects.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { curPage } from "../../stores.js";
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <div class="row">
+        <div class="item">
+            <h1>hello</h1>
+        </div>
+        <div class="item">
+            <h1>hello</h1>
+        </div>
+    </div>
+</div>
