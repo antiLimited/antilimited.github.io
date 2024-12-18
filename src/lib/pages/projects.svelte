0ddde2795764c2b7989acab3ff7e1810cfe7c2bf
antiLimit <webmaster@antilimit.dev>
2024-12-18 11:15:39 -0500

    not working but i need to commit RIGHT now :3

--- src/lib/pages/projects.svelte
+++ src/lib/pages/projects.svelte
@@ -1,13 +1,31 @@
 <script>
     import { curPage } from "../../stores.js";
+
+    const isElementLoaded = async (selector) => {
+        while (document.querySelector(selector) === null) {
+            await new Promise((resolve) => requestAnimationFrame(resolve));
+        }
+        return document.querySelector(selector);
+    };
+
+    let colCount = $state();
+
+    isElementLoaded(".grid").then((selector) => {
+        colCount = selector.children.length;
+        for (let i = 0;)
+    });
 </script>
 
-<style>
-
-</style>
-
-<div>
-    <div class="row">
+<div class="grid">
+    <div class="col">
+        <div class="item">
+            <h1>hello</h1>
+        </div>
+        <div class="item">
+            <h1>hello</h1>
+        </div>
+    </div>
+    <div class="col">
         <div class="item">
             <h1>hello</h1>
         </div>
@@ -16,3 +34,13 @@
         </div>
     </div>
 </div>
+
+<style>
+    .grid {
+        display: flex;
+    }
+
+    .col {
+        flex: 1;
+    }
+</style>
