0d29fbfc3d6b5478198777d8e932acc45e3eb5a6
antiLimit <webmaster@antilimit.dev>
2024-12-20 00:32:16 -0500

    final commit for night some progress on projects page :3

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -6,19 +6,25 @@
     let navbarLinks = [];
 
     // Runs on page load
-    document.addEventListener('DOMContentLoaded', function() {
-        console.log("DOM loaded!");
-        navbar = document.querySelector(".navbar"); // Sets navbar to refer to top level navbar div
-        
-        if (window.scrollY == 0) {
-            navbar.classList.add("navbar-init-up"); // Set starting position docked on init (no anim)
-        } else {
-            navbar.classList.add("navbar-init-down"); // Set starting position floating on init (no anim)
-        };
+    document.addEventListener(
+        "DOMContentLoaded",
+        function () {
+            console.log("DOM loaded!");
+            navbar = document.querySelector(".navbar"); // Sets navbar to refer to top level navbar div
 
-        navbarLinks = Array.from(document.getElementsByClassName("navbar-link")); // Set navbarLinks to an array of all links in top level navbar div
-        highlightCurPage(true); // Call highlight with init true to prevent animation for inactive links
-    }, false);
+            if (window.scrollY == 0) {
+                navbar.classList.add("navbar-init-up"); // Set starting position docked on init (no anim)
+            } else {
+                navbar.classList.add("navbar-init-down"); // Set starting position floating on init (no anim)
+            }
+
+            navbarLinks = Array.from(
+                document.getElementsByClassName("navbar-link"),
+            ); // Set navbarLinks to an array of all links in top level navbar div
+            highlightCurPage(true); // Call highlight with init true to prevent animation for inactive links
+        },
+        false,
+    );
 
     // Call on any scroll events on page
     addEventListener("scroll", () => {
@@ -30,7 +36,7 @@
             // Set navbar classes to appear floating (down) otherwise
             navbar.classList.add("navbar-down");
             navbar.classList.remove("navbar-up");
-        };
+        }
     });
 
     // Highlight active page link in navbar based on $curPage store
@@ -41,7 +47,10 @@
 
             // Check if current page link matches $curPage
             if ($curPage == link.innerHTML.toLowerCase()) {
-                link.classList.remove("navbar-inactive", "navbar-inactive-pgload"); // Remove old inactive classes
+                link.classList.remove(
+                    "navbar-inactive",
+                    "navbar-inactive-pgload",
+                ); // Remove old inactive classes
                 link.classList.add("navbar-active"); // Add active class
             } else {
                 link.classList.remove("navbar-active"); // Remove old active class
@@ -49,17 +58,23 @@
                     link.classList.add("navbar-inactive-pgload"); // Added if first call after DOM load, prevents active to inactive anim on page load
                 } else {
                     link.classList.add("navbar-inactive"); // Added on any other call
-                };
-            };
-        };
-    };
+                }
+            }
+        }
+    }
     $effect(() => highlightCurPage(false, $curPage)); // Re-highlight page when $curPage store is changed
 </script>
 
 <div class="navbar">
-    <a href="#" class="navbar-link" onclick={() => $curPage = "home"}>HOME</a>
+    <a href="#" class="navbar-link" onclick={() => ($curPage = "home")}
+        >HOME
+    </a>
     <div class="navbar-sep"></div>
-    <a href="#" class="navbar-link" onclick={() => $curPage = "profiles"}>PROFILES</a>
+    <a href="#" class="navbar-link" onclick={() => ($curPage = "profiles")}
+        >PROFILES</a
+    >
     <div class="navbar-sep"></div>
-    <a href="#" class="navbar-link" onclick={() => $curPage = "projects"}>PROJECTS</a>
+    <a href="#" class="navbar-link" onclick={() => ($curPage = "projects")}
+        >PROJECTS</a
+    >
 </div>
--- src/lib/pages/projects.svelte
+++ src/lib/pages/projects.svelte
@@ -5,6 +5,24 @@
 <div class="grid">
     <div class="col">
         <div class="item">
+            <h1>big ass store</h1>
+            <img src="src/assets/pages/projects/rt2store.png" alt="" />
+            <a href="https://www.roblox.com/games/5865858426/Retail-Tycoon-2"
+                >Check it out here</a
+            >
+            <p>
+                haha look at this big ass store its got the helipads and all
+                this stuff i made it in retail tycoon 2 also im typing for the
+                sake of typing because i need this as an example description
+                okay thanks byeeeeee
+            </p>
+        </div>
+        <div class="item">
+            <h1>hello</h1>
+        </div>
+    </div>
+    <div class="col">
+        <div class="item">
             <h1>hello</h1>
         </div>
         <div class="item">
@@ -27,14 +45,56 @@
         flex-direction: row;
         flex-wrap: wrap;
         flex-basis: 100%;
-        align-items: stretch;
-
-        margin: 0%;
+        align-self: flex-start;
     }
 
     .col {
-        align-items: center;
-
         flex: 1;
+        justify-content: center;
+    }
+
+    .item {
+        flex-wrap: nowrap;
+
+        margin: 20px 10px;
+        padding: 10px;
+        border-radius: 15px;
+
+        background-color: var(--foreground-color);
+    }
+
+    .item h1 {
+        display: flex;
+        justify-content: center;
+
+        margin: 0;
+        margin-bottom: 10px;
+        border-radius: 15px;
+
+        font-size: 25pt;
+        text-align: center;
+
+        background-color: var(--background-color);
+    }
+
+    .item img {
+        display: flex;
+
+        margin-bottom: 10px;
+        max-width: 100%;
+        border-radius: 15px;
+    }
+
+    .item p {
+        display: flex;
+        justify-content: center;
+
+        margin: 0;
+        padding: 10px;
+        border-radius: 15px;
+
+        text-align: center;
+
+        background-color: var(--background-color);
     }
 </style>
